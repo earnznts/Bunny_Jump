1a6a7e6ecf1f0195d08afde3b3c9a6c818285afc
earnztns <aern5731zaz@gmail.com>
2019-09-19 11:43:53 +0700

    UPDATE 19/09/2019

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,24 +1,35 @@
 extends KinematicBody2D
 
-var motion = Vector2(0,100)
-	
+var motion = Vector2(0,0)
+
+export var Gravity = 65
 export var SPEED = 500
-export var JUMP = 710
+export var JUMP = 1000
 
 func _physics_process(delta):
+	
+	apply_gravity()
+	jump()
+	walk()
+	
+	move_and_slide(motion,Vector2( 0, -1))
+	
+func apply_gravity():
+	if is_on_floor():
+		motion.y = 0
+		#print("I'm floor")
+	motion.y = motion.y + Gravity
+
+func jump():
+	if Input.is_action_just_pressed("jump"):
+		motion.y = -JUMP
+	else: 
+		motion.y
+
+func walk():
 	if  Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
 		motion.x = -SPEED
 	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
 		motion.x = SPEED
 	else: 
 		motion.x = 0
-		
-	if Input.is_action_pressed("jump"):
-		motion.y = -JUMP
-	else: 
-		motion.y = 600
-		
-	move_and_slide(motion)
-		
-		
-		
