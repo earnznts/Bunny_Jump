d5069c2d848318ff6c2ca4300f5e3c1370d5e560
earnztns <aern5731zaz@gmail.com>
2019-09-19 12:28:56 +0700

    Update Double Jump

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,6 +5,7 @@ var motion = Vector2(0,0)
 export var Gravity = 65
 export var SPEED = 500
 export var JUMP = 1000
+export var Jump_Count = 0
 
 func _physics_process(delta):
 	
@@ -15,16 +16,19 @@ func _physics_process(delta):
 	move_and_slide(motion,Vector2( 0, -1))
 	
 func apply_gravity():
+	motion.y = motion.y + Gravity
 	if is_on_floor():
 		motion.y = 0
 		#print("I'm floor")
-	motion.y = motion.y + Gravity
+	
 
 func jump():
-	if Input.is_action_just_pressed("jump"):
-		motion.y = -JUMP
-	else: 
-		motion.y
+	if Jump_Count < 1:
+		if Input.is_action_just_pressed("jump"):
+			motion.y = -JUMP
+			Jump_Count = Jump_Count + 1
+	if is_on_floor():
+		Jump_Count = 0
 
 func walk():
 	if  Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
